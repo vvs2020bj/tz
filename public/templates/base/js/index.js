2e6097a596c01a529fce8697eb0f4383e062f3c5
vvs2020bj <74403687+vvs2020bj@users.noreply.github.com>
2020-11-13 11:23:41 +0200

    Add files via upload

---
+++ public/templates/base/js/index.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+	load_add_task_form();
+	auth_form();
+
+	$(window).resize(function() {});
+
+});
+
+
+
+
+
+load_add_task_form = function()
+{
+	$('#add_task button').on({
+		click: function() {
+			
+			$.post('/index/get_form_task_add', {}, function(data) {
+				history.pushState(null, null, '/');
+				$('#body').html(data);
+				$('input[name=name]').focus();
+				task_save();
+			});
+
+		}
+	});
+};
+
+
+task_save = function()
+{
+	$('#task_save button').on({
+		click: function() {
+			var data = $('#task-add-block').serializeArray();
+
+			$.post('/index/save_task_add', data, function(data) {
+				$('#body').html(data);
+				load_add_task_form();
+			});
+
+		}
+	});
+};
+
+
+auth_form = function()
+{
+	$('#login').on({
+		click: function() {
+			
+			$.post('/index/get_form_login', {}, function(data) {
+				history.pushState(null, null, '/');
+				$('#body').html(data);
+				$('input[name=login]').focus();
+				login();
+			});
+
+		}
+	});
+};
+
+
+login = function()
+{
+	$('#task_save button').on({
+		click: function() {
+			var data = $('#auth-block').serializeArray();
+
+			$.post('/index/login', data, function(data) {
+				$('#body').html(data);
+				auth_form();
+			});
+
+		}
+	});
+};
